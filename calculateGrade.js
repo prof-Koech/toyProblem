741e293647bd5d2b281e55be348dadda18158846
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-28 17:15:39 +0300

    done

--- calculateGrade.js
+++ calculateGrade.js
@@ -1,17 +1,26 @@
-function calculateGrade() {
-  const marks = parseInt(prompt("Enter the student's marks (between 0 and 100):"));
 
-  if (marks >= 80 && marks <=100) { //checks if marks is between range of 80 and 100
-    return 'A';
-  } else if (marks >= 60 && marks <80) {//checks if marks is between range of 60 and 79
-    return 'B';
-  } else if (marks >= 50 && marks < 60) {//checks if marks is between range of 50 and 59
-    return 'C';
-  } else if (marks >= 40 && marks <50) {//checks if marks is between range of 40 and 49
-    return 'D';
-  } else if (marks<40 && marks >=0) {  //checks if marks is between range of 0 and 39
-    return 'E';
-  }else {
-    return 'invalid score'; //checks if the marks is outside the range
-  }
+
+function studentGrade1(){
+  let marks = prompt("Enter a student Grade : ");
+  let grade;
+  marks=parseFloat(marks);//it takes in student marks as float values cause marks may contain a point
+switch(marks){
+  case (marks>79):
+    grade= 'A';
+    break;
+  case (marks>60 && marks<=79):
+    grade= 'B';
+    break;
+  case (marks>50 && marks<60):
+    grade= 'C';
+    break;
+  case (marks>40 && marks<=49):
+    grade= 'D';
+    break;
+  default:
+    grade= 'E';
+  
 }
+return `The student's grade is ${grade}`;
+}
+console.log(studentGrade1())
